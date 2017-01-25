c44570d337f94cc943503674639a74fbccafd13f
Bryan Woods <bryanwoods4e@gmail.com>
2017-01-24 23:38:48 -0500

    Copy static files to root for github pages

---
+++ js/release/website/prod.js
@@ -0,0 +1,26 @@
+// Compiled by ClojureScript 1.9.229 {:static-fns true, :optimize-constants true}
+goog.provide('website.prod');
+goog.require('cljs.core');
+goog.require('website.core');
+cljs.core._STAR_print_fn_STAR_ = (function() { 
+var G__12973__delegate = function (_){
+return null;
+};
+var G__12973 = function (var_args){
+var _ = null;
+if (arguments.length > 0) {
+var G__12974__i = 0, G__12974__a = new Array(arguments.length -  0);
+while (G__12974__i < G__12974__a.length) {G__12974__a[G__12974__i] = arguments[G__12974__i + 0]; ++G__12974__i;}
+  _ = new cljs.core.IndexedSeq(G__12974__a,0);
+} 
+return G__12973__delegate.call(this,_);};
+G__12973.cljs$lang$maxFixedArity = 0;
+G__12973.cljs$lang$applyTo = (function (arglist__12975){
+var _ = cljs.core.seq(arglist__12975);
+return G__12973__delegate(_);
+});
+G__12973.cljs$core$IFn$_invoke$arity$variadic = G__12973__delegate;
+return G__12973;
+})()
+;
+website.core.init_BANG_();
