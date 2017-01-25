9267b33541bd0de71c02a96de2841348d6530cf0
Bryan Woods <bryanwoods4e@gmail.com>
2017-01-25 10:56:35 -0500

    Makes things a little nicer

--- public/js/release/reagent/debug.js
+++ public/js/release/reagent/debug.js
@@ -12,48 +12,48 @@ if(typeof reagent.debug.track_console !== 'undefined'){
 reagent.debug.track_console = (function (){var o = ({});
 o.warn = ((function (o){
 return (function() { 
-var G__12318__delegate = function (args){
+var G__12275__delegate = function (args){
 return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$variadic(reagent.debug.warnings,cljs.core.update_in,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$warn], null),cljs.core.conj,cljs.core.array_seq([cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.str,args)], 0));
 };
-var G__12318 = function (var_args){
+var G__12275 = function (var_args){
 var args = null;
 if (arguments.length > 0) {
-var G__12319__i = 0, G__12319__a = new Array(arguments.length -  0);
-while (G__12319__i < G__12319__a.length) {G__12319__a[G__12319__i] = arguments[G__12319__i + 0]; ++G__12319__i;}
-  args = new cljs.core.IndexedSeq(G__12319__a,0);
+var G__12276__i = 0, G__12276__a = new Array(arguments.length -  0);
+while (G__12276__i < G__12276__a.length) {G__12276__a[G__12276__i] = arguments[G__12276__i + 0]; ++G__12276__i;}
+  args = new cljs.core.IndexedSeq(G__12276__a,0);
 } 
-return G__12318__delegate.call(this,args);};
-G__12318.cljs$lang$maxFixedArity = 0;
-G__12318.cljs$lang$applyTo = (function (arglist__12320){
-var args = cljs.core.seq(arglist__12320);
-return G__12318__delegate(args);
+return G__12275__delegate.call(this,args);};
+G__12275.cljs$lang$maxFixedArity = 0;
+G__12275.cljs$lang$applyTo = (function (arglist__12277){
+var args = cljs.core.seq(arglist__12277);
+return G__12275__delegate(args);
 });
-G__12318.cljs$core$IFn$_invoke$arity$variadic = G__12318__delegate;
-return G__12318;
+G__12275.cljs$core$IFn$_invoke$arity$variadic = G__12275__delegate;
+return G__12275;
 })()
 ;})(o))
 ;
 
 o.error = ((function (o){
 return (function() { 
-var G__12321__delegate = function (args){
+var G__12278__delegate = function (args){
 return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$variadic(reagent.debug.warnings,cljs.core.update_in,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$error], null),cljs.core.conj,cljs.core.array_seq([cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.str,args)], 0));
 };
-var G__12321 = function (var_args){
+var G__12278 = function (var_args){
 var args = null;
 if (arguments.length > 0) {
-var G__12322__i = 0, G__12322__a = new Array(arguments.length -  0);
-while (G__12322__i < G__12322__a.length) {G__12322__a[G__12322__i] = arguments[G__12322__i + 0]; ++G__12322__i;}
-  args = new cljs.core.IndexedSeq(G__12322__a,0);
+var G__12279__i = 0, G__12279__a = new Array(arguments.length -  0);
+while (G__12279__i < G__12279__a.length) {G__12279__a[G__12279__i] = arguments[G__12279__i + 0]; ++G__12279__i;}
+  args = new cljs.core.IndexedSeq(G__12279__a,0);
 } 
-return G__12321__delegate.call(this,args);};
-G__12321.cljs$lang$maxFixedArity = 0;
-G__12321.cljs$lang$applyTo = (function (arglist__12323){
-var args = cljs.core.seq(arglist__12323);
-return G__12321__delegate(args);
+return G__12278__delegate.call(this,args);};
+G__12278.cljs$lang$maxFixedArity = 0;
+G__12278.cljs$lang$applyTo = (function (arglist__12280){
+var args = cljs.core.seq(arglist__12280);
+return G__12278__delegate(args);
 });
-G__12321.cljs$core$IFn$_invoke$arity$variadic = G__12321__delegate;
-return G__12321;
+G__12278.cljs$core$IFn$_invoke$arity$variadic = G__12278__delegate;
+return G__12278;
 })()
 ;})(o))
 ;
--- src/website/core.cljs
+++ src/website/core.cljs
@@ -1,5 +1,5 @@
 (ns website.core
-    (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]))
 
 ;; -------------------------
 ;; Views
@@ -11,48 +11,45 @@
 
 (defn online-component []
   [section-component "Online"
-    [:p
-      [:a {:href "http://twitter.com/bryanwoods"} "TWITTER"]]
-    [:p
-      [:a {:href "mailto:bryanwoods4e@gmail.com"} "GMAIL"]]])
+   [:p
+    [:a {:href "http://twitter.com/bryanwoods"} "TWITTER"]]
+   [:p
+    [:a {:href "mailto:bryanwoods4e@gmail.com"} "GMAIL"]]])
 
 (defn writing-component []
   [section-component "Writing"
-    [:p
-      [:a {:href "http://thefanzine.com/experience/"} "EXPERIENCE"]
-     ", The Fanzine"]
-    [:p
-      [:a {:href "http://potluckmag.com/february-2016/2016/2/4/bathtime-disintegration"}
-       "BATHTIME DISINTEGRATION"]
-     ", Potluck"]
-    [:p
-      [:a {:href "http://queenmobs.com/2015/12/poem-bryan-woods/"}
-       "DISINTEGRATIONS"]
-     ", Queen Mob's Tea House"]])
+   [:p
+    [:a {:href "http://thefanzine.com/experience/"} "EXPERIENCE"]
+    ", The Fanzine"]
+   [:p
+    [:a {:href "http://potluckmag.com/february-2016/2016/2/4/bathtime-disintegration"}
+     "BATHTIME DISINTEGRATION"]
+    ", Potluck"]
+   [:p
+    [:a {:href "http://queenmobs.com/2015/12/poem-bryan-woods/"}
+     "DISINTEGRATIONS"]
+    ", Queen Mob's Tea House"]])
 
 (defn programming-component []
   [section-component "Programming"
-    [:p
-      [:a {:href "https://techcrunch.com/2010/10/29/facebook-acquires-drop-io-nabs-sam-lessin/"} "MANY"]
-      " "
-      [:a {:href "http://www.businessinsider.com/howaboutwe-iac-acquisition-2014-6"} "START"]
-      " "
-      [:a {:href "http://www.psdept.com"} "UPS"]
-      " & "
-      [:a {:href "http://www.samsung.com"} "AN INTERNATIONAL CORPORATION"]
-      " & "
-      [:a {:href "http://github.com/bryanwoods"} "OPEN SOURCE"]]
-    [:p
-      "This web page was written in Clojure, a dialect of Lisp, a language 
-      'discovered' in the 1950s but which runs on the Java Virtual Machine, before
-      I compiled it to JavaScript and rendered it with the React web framework."
-      [:br]
-      "Not because it's practical, but because it's funny."
-      [:br]
-      "You can read the source code "
-      [:a {:href "https://github.com/bryanwoods/website/blob/master/src/website/core.cljs"} "here"]
-       "."
-     ]])
+   [:p
+    [:a {:href "https://techcrunch.com/2010/10/29/facebook-acquires-drop-io-nabs-sam-lessin/"} "MANY"]
+    " "
+    [:a {:href "http://www.businessinsider.com/howaboutwe-iac-acquisition-2014-6"} "START"]
+    " "
+    [:a {:href "http://www.psdept.com"} "UPS"]
+    " & "
+    [:a {:href "http://www.samsung.com"} "AN INTERNATIONAL CORPORATION"]
+    " & "
+    [:a {:href "http://github.com/bryanwoods"} "OPEN SOURCE"]]
+   [:p
+    "This web page was written in Clojure, compiled to JavaScript, and rendered with React."
+    [:br]
+    "Not because it's practical, but because it's funny."
+    [:br]
+    "You can read its source code "
+    [:a {:href "https://github.com/bryanwoods/website/blob/master/src/website/core.cljs"} "here"]
+    "."]])
 
 (defn music-component []
   [section-component "Music"
@@ -66,11 +63,11 @@
 
 (defn home-page []
   [:div
-    [:h1 "Bryan Woods"]
-    [online-component]
-    [writing-component]
-    [programming-component]
-    [music-component]])
+   [:h1 "Bryan Woods"]
+   [online-component]
+   [writing-component]
+   [programming-component]
+   [music-component]])
 
 ;; -------------------------
 ;; Initialize app
