0bdaa7ace21dc96b7f3af109f92d1257aa4fa37a
Bryan Woods <bryanwoods4e@gmail.com>
2017-01-24 20:28:51 -0500

    Makes a website

---
+++ src/website/core.cljs
@@ -0,0 +1,82 @@
+(ns website.core
+    (:require [reagent.core :as reagent]))
+
+;; -------------------------
+;; Views
+
+(defn section-component [header & body]
+  (into [:div
+         [:strong header]]
+        body))
+
+(defn online-component []
+  [section-component "Online"
+    [:p
+      [:a {:href "http://twitter.com/bryanwoods"} "TWITTER"]]
+    [:p
+      [:a {:href "mailto:bryanwoods4e@gmail.com"} "GMAIL"]]])
+
+(defn writing-component []
+  [section-component "Writing"
+    [:p
+      [:a {:href "http://thefanzine.com/experience/"} "EXPERIENCE"]
+     ", The Fanzine"]
+    [:p
+      [:a {:href "http://potluckmag.com/february-2016/2016/2/4/bathtime-disintegration"}
+       "BATHTIME DISINTEGRATION"]
+     ", Potluck"]
+    [:p
+      [:a {:href "http://queenmobs.com/2015/12/poem-bryan-woods/"}
+       "DISINTEGRATIONS"]
+     ", Queen Mob's Tea House"]])
+
+(defn programming-component []
+  [section-component "Programming"
+    [:p
+      [:a {:href "https://techcrunch.com/2010/10/29/facebook-acquires-drop-io-nabs-sam-lessin/"} "MANY"]
+      " "
+      [:a {:href "http://www.businessinsider.com/howaboutwe-iac-acquisition-2014-6"} "START"]
+      " "
+      [:a {:href "http://www.psdept.com"} "UPS"]
+      " & "
+      [:a {:href "http://www.samsung.com"} "AN INTERNATIONAL CORPORATION"]
+      " & "
+      [:a {:href "http://github.com/bryanwoods"} "OPEN SOURCE"]]
+    [:p
+      "This web page was written in Clojure, a dialect of Lisp, a language 
+      'discovered' in the 1950s but which runs on the Java Virtual Machine, before
+      I compiled it to JavaScript and rendered it with the React web framework."
+      [:br]
+      "Not because it's practical, but because it's funny."
+      [:br]
+      "You can read the source code "
+      [:a {:href "https://github.com/bryanwoods/website/blob/master/src/website/core.cljs"} "here"]
+       "."
+     ]])
+
+(defn music-component []
+  [section-component "Music"
+   [:p
+    [:a {:href "https://thebirdcagetheater.bandcamp.com/"} "BANDCAMP"]]
+   [:p
+    [:a {:href "https://www.discogs.com/Bird-Cage-Theater-Finite-CityHurricane-Dirge/release/5431765"}
+     "DISCOGS"]
+    [:p
+     [:a {:href "https://vimeo.com/14313644"} "VIMEO"]]]])
+
+(defn home-page []
+  [:div
+    [:h1 "Bryan Woods"]
+    [online-component]
+    [writing-component]
+    [programming-component]
+    [music-component]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (reagent/render [home-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
