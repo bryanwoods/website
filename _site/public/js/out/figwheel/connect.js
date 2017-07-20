f633e64e3f2030ff3dba4bc1a363212c2fa14b78
Bryan Woods <bryanwoods4e@gmail.com>
2017-07-19 20:43:03 -0400

    Removing clojurescript monstrosity

--- _site/public/js/out/figwheel/connect.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.9.229 {}
-goog.provide('figwheel.connect');
-goog.require('cljs.core');
-goog.require('website.dev');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"open-urls","open-urls",-1478664930),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["http://localhost:3449/index.html"], null),new cljs.core.Keyword(null,"build-id","build-id",1642831089),"app",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
-
-//# sourceMappingURL=connect.js.map?rel=1499477035615
